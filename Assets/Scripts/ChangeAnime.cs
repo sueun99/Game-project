4bd79168a6f9dfb33c64ad98b2847867100d8800
sueun99 <gjtndms11352@gmail.com>
2024-05-25 03:50:45 +0900

    title etc

--- Assets/Scripts/ChangeAnime.cs
+++ Assets/Scripts/ChangeAnime.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,26 +9,25 @@ public class ChangeAnime : MonoBehaviour
     public string downAnime = "";
     public string rightAnime = "";
     public string leftAnime = "";
+    public string idleAnime = "";
 
     string nowMode = "";
 
     bool pushFlag = false; // 스페이스 키가 눌린 상태인지 
     bool jumpFlag = false; // 점프 상태인지 
     bool groundFlag = false; // 발이 무언가에 닿았는지 
-    Rigidbody2D rbody;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        nowMode = downAnime;
-        rbody = GetComponent<Rigidbody2D>();
-        rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        nowMode = idleAnime; // 시작 시 애니메이션
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("space") && groundFlag)
+        if (Input.GetKey("space") && groundFlag)
         {
             if (pushFlag == false)// 계속 누르고 나가지 않으면
             {
@@ -40,21 +40,36 @@ public class ChangeAnime : MonoBehaviour
             pushFlag = false; // 계속 누른 상태 해제 
         }
 
-        if (Input.GetKey("space") && jumpFlag == false)
+        if (groundFlag == false)
         {
             nowMode = upAnime;
         }
-        if (Input.GetKey("down") && jumpFlag == true)
+        else
         {
-            nowMode = downAnime;
-        }
-        if (Input.GetKey("right") && jumpFlag == true)
-        {
-            nowMode = rightAnime;
-        }
-        if (Input.GetKey("left") && jumpFlag == true)
-        {
-            nowMode = leftAnime;
+            if (Input.GetKey("down"))
+            {
+                nowMode = downAnime;
+            }
+            if (Input.GetKey("right"))
+            {
+                nowMode = rightAnime;
+            }
+            if (Input.GetKey("left"))
+            {
+                nowMode = leftAnime;
+            }
+            if (Input.GetKeyUp("down"))
+            {
+                nowMode = idleAnime;
+            }
+            if (Input.GetKeyUp("right"))
+            {
+                nowMode = idleAnime;
+            }
+            if (Input.GetKeyUp("left"))
+            {
+                nowMode = idleAnime;
+            }
         }
     }
 
@@ -72,6 +87,7 @@ public class ChangeAnime : MonoBehaviour
     void OnTriggerStay2D(Collider2D collision)
     { // 발이 무언가에 닿으면 
         groundFlag = true;
+        nowMode = idleAnime;
     }
     void OnTriggerExit2D(Collider2D collision)
     { // 발에 아무 것도 닿지 않으면 
